8943f2ddc452477d7cc2ce373fb45c7efe4e4d88
Zetnavrek <vykovi@gmail.com>
2018-05-17 11:53:29 -0500

    Se suben a repositorio cambios en el SaveButton y focus

--- js/app.js
+++ js/app.js
@@ -9,21 +9,8 @@ transform.addEventListener('click', function(){
 
 });
 
-var saveListButton=document.getElementById("save-list");//Creando el evento para el boton que le pone el nombre al titulo y lo mustra. Ocultar el formulario.
-saveListButton.addEventListener('click', function(){
-var saveValue=document.getElementById("get-value");
-var ListButton=saveValue.value;
-var contentValue=document.createTextNode(ListButton);
-var father=document.getElementById("title-list");
-var tasksTitle=document.getElementById("tasks-title");
-tasksTitle.appendChild(contentValue);
-// father.appendChild(tasksTitle);
-tasksTitle.insertBefore(tasksTitle, saveValue );
-});
 
-   // }
-
-//Version 2.0 
+//Version 3.0 - 4.0 - 5.0
 var newTaskButton = document.getElementById("save-task");
 newTaskButton.addEventListener('click', function(){
     var TaskContent = document.getElementById("taskContent");
@@ -31,9 +18,19 @@ newTaskButton.addEventListener('click', function(){
     var newParagraph = document.createElement("p");
     var textNewContent = document.createTextNode(content);
     newParagraph.appendChild(textNewContent);
-    document.getElementById("container").appendChild(newParagraph); //Se inserta el "p" creado dentro del div contenedor, y se muestra en pantalla.
-});   
+    document.getElementById("container").appendChild(newParagraph);  //Se inserta el "p" creado dentro del div contenedor, y se muestra en pantalla.
+    TaskContent.focus(); // Se inserta focus en el textarea.
+});                                         
 
-
-
-
+    var saveListButton=document.getElementById("save-list");//Creando el evento para el boton que le pone el nombre al titulo y lo mustra. Ocultar el formulario.
+    saveListButton.addEventListener('click', function(){
+    var saveValue=document.getElementById("get-value");
+    var ListButton=saveValue.value;
+    var contentValue=document.createTextNode(ListButton);
+    var father=document.getElementById("title-list");
+    var tasksTitle=document.getElementById("tasks-title");
+    tasksTitle.appendChild(contentValue);
+    // father.appendChild(tasksTitle);
+    tasksTitle.insertBefore(tasksTitle, saveValue );
+    });
+    
