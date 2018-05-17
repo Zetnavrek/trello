fe4e670c21ec783c98f03dda3f94cf87ab531a9e
Zetnavrek <vykovi@gmail.com>
2018-05-17 15:01:22 -0500

    Se actualizan los cambios a lo ya realizado y que ya era funcional, en el intento por realizar la version 2.0

--- js/app.js
+++ js/app.js
@@ -2,9 +2,35 @@
 //Se crea un evento para que aparezca el text area, y desaparezca el primer input. 
 var transform = document.getElementById ("list");
 transform.addEventListener('click', function(){
-    var lista1 = document.getElementById("title-list");
+    var lista1 = document.getElementById("version2");
     lista1.style.display= "block";
     transform.style.display="none";
+});
+
+//Intento version 2.0
+var saveListButton=document.getElementById("save-list");//Creando el evento para el boton que le pone el nombre al titulo y lo mustra. Ocultar el formulario.
+    saveListButton.addEventListener('click', function(){
+var saveValue=document.getElementById("get-value");
+var ListButton=saveValue.value;
+var contentValue=document.createTextNode(ListButton);
+var father=document.getElementById("title-list");
+var tasksTitle=document.getElementById("tasks-title");
+var showList=document.getElementById("show-list");//Se intenta agregar la liga de lista de tareas.
+var newSaveTask=document.getElementById("title-list");
+tasksTitle.appendChild(contentValue);
+// father.appendChild(tasksTitle);
+//tasksTitle.insertBefore(tasksTitle, "save-list");
+saveListButton.style.display='none';
+showList.style.display="block";
+
+showList.addEventListener('click', function(){
+    var nextList= document.getElementById("title-list");
+
+});
+
+
+
+
 });
 
 
@@ -18,17 +44,5 @@ newTaskButton.addEventListener('click', function(){
     newParagraph.appendChild(textNewContent);
     document.getElementById("container").appendChild(newParagraph);  //Se inserta el "p" creado dentro del div contenedor, y se muestra en pantalla.
     TaskContent.focus(); // Se inserta focus en el textarea.
-});                                         
-//Intento version 2.0
-    var saveListButton=document.getElementById("save-list");//Creando el evento para el boton que le pone el nombre al titulo y lo mustra. Ocultar el formulario.
-    saveListButton.addEventListener('click', function(){
-    var saveValue=document.getElementById("get-value");
-    var ListButton=saveValue.value;
-    var contentValue=document.createTextNode(ListButton);
-    var father=document.getElementById("title-list");
-    var tasksTitle=document.getElementById("tasks-title");
-    tasksTitle.appendChild(contentValue);
-    // father.appendChild(tasksTitle);
-    tasksTitle.insertBefore(tasksTitle, "save-list");
-    });
-    
+});                          
+
