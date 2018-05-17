59ddb3c10fc17a0cebf4765a11fe5d6d26c1b472
Zetnavrek <vykovi@gmail.com>
2018-05-17 12:21:45 -0500

    Ligeros cambios de explicacion y comentarios dentro del codigo

--- js/app.js
+++ js/app.js
@@ -1,27 +1,25 @@
 // Version 0.1 
 //Se crea un evento para que aparezca el text area, y desaparezca el primer input. 
-
 var transform = document.getElementById ("list");
 transform.addEventListener('click', function(){
     var lista1 = document.getElementById("title-list");
     lista1.style.display= "block";
     transform.style.display="none";
-
 });
 
 
 //Version 3.0 - 4.0 - 5.0
 var newTaskButton = document.getElementById("save-task");
 newTaskButton.addEventListener('click', function(){
-    var TaskContent = document.getElementById("taskContent");
-    var content = TaskContent.value; //Se guarda valor del input en la variable.
-    var newParagraph = document.createElement("p");
+    var TaskContent=document.getElementById("taskContent");
+    var content=TaskContent.value; //Se guarda valor del input en la variable.
+    var newParagraph=document.createElement("p");
     var textNewContent = document.createTextNode(content);
     newParagraph.appendChild(textNewContent);
     document.getElementById("container").appendChild(newParagraph);  //Se inserta el "p" creado dentro del div contenedor, y se muestra en pantalla.
     TaskContent.focus(); // Se inserta focus en el textarea.
 });                                         
-
+//Intento version 2.0
     var saveListButton=document.getElementById("save-list");//Creando el evento para el boton que le pone el nombre al titulo y lo mustra. Ocultar el formulario.
     saveListButton.addEventListener('click', function(){
     var saveValue=document.getElementById("get-value");
@@ -31,6 +29,6 @@ newTaskButton.addEventListener('click', function(){
     var tasksTitle=document.getElementById("tasks-title");
     tasksTitle.appendChild(contentValue);
     // father.appendChild(tasksTitle);
-    tasksTitle.insertBefore(tasksTitle, saveValue );
+    tasksTitle.insertBefore(tasksTitle, "save-list");
     });
     
